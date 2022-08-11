bb30dfa4d96a417e86dcb543db0fcb946c35a033
wangjl <1428637884@qq.com>
2022-08-11 16:39:04 +0800

    测试分支是否运行正常

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@
  * @Author: JunLiangWang
  * @Date: 2022-08-08 15:48:22
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2022-08-11 15:28:55
+ * @LastEditTime: 2022-08-11 16:38:48
  */
 module.exports = {
     // 基本配置 https://www.vuepress.cn/config/#%E5%9F%BA%E6%9C%AC%E9%85%8D%E7%BD%AE
